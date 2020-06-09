64e9299c289e32699e79cb4b04ee9cab27a73660
alibeyI <alibey.ibrahimli@gmail.com>
2020-06-09 16:38:44 +0400

    Updates

---
+++ precache-manifest.a16ba49ea5f121fe9a373cd4fe4753c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ea4585eaa8c2e8b0e6cc198598d0e17",
+    "url": "/rnmortyFav/index.html"
+  },
+  {
+    "revision": "dce285f9f76cbf3b7466",
+    "url": "/rnmortyFav/static/css/main.980f7af8.chunk.css"
+  },
+  {
+    "revision": "6c6f3dd3c580e6dca537",
+    "url": "/rnmortyFav/static/js/0.e59cde0c.chunk.js"
+  },
+  {
+    "revision": "a8a8db01ab5cd001d4bb",
+    "url": "/rnmortyFav/static/js/3.ec34271d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rnmortyFav/static/js/3.ec34271d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dce285f9f76cbf3b7466",
+    "url": "/rnmortyFav/static/js/main.34569d79.chunk.js"
+  },
+  {
+    "revision": "9a757290baa622f01baa",
+    "url": "/rnmortyFav/static/js/runtime-main.00213b0a.js"
+  }
+]);
